62060f808ae882ca886a9e441f8beee0c9e2f8bd
aitsereal1973 <odidoquinom@hotmail.com>
2025-09-03 12:07:24 -0500

    Update welcome message for clarity.

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Welcome to Your New Recipe Adventure!</Text>
+      <Text>Welcome to Your New Recipe Journey!</Text>
     </View>
   );
 };
