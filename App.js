6c2be1481069f0effec3e3b9aa81bc65fa1b5142
aitsereal1973 <odidoquinom@hotmail.com>
2025-09-03 12:04:48 -0500

    feat: initial commit for App.js

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Bienvenido a la App de Recetas!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+});
+
+export default App;
